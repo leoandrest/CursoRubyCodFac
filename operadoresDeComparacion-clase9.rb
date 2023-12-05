59cb2f16de08ae47ee2735d6d52b0201a8f19fcf
leo tatico <leoandrest40@gmail.com>
2023-12-05 12:48:03 -0500

    add clase 9 operadores de comparacion

---
+++ operadoresDeComparacion-clase9.rb
@@ -0,0 +1,35 @@
+# En Ruby, los operadores de comparación se utilizan para comparar 
+#dos valores y devolver un resultado booleano (verdadero o falso). Aquí están los operadores de comparación en Ruby:
+
+# igualdad 
+resultado = 5 == 5  # resultado será true
+
+# desigualdad 
+resultado = 5 != 3  # resultado será true
+
+# mayor que (>) 
+resultado = 5 > 3  # resultado será true
+# menor que (<)
+resultado = 5 < 3  # resultado será false
+# mayor o igual que (>=)
+resultado = 5 >= 5  # resultado será true
+# menor o igual que (<=)
+resultado = 5 <= 3  # resultado será false
+
+# Los operadores de comparación se utilizan comúnmente en estructuras de control como 
+# if y while para tomar decisiones basadas en las relaciones entre valores.
+
+# ej:
+
+a = 5
+b = 3
+
+if a > b
+  puts "a es mayor que b"
+else
+  puts "a no es mayor que b"
+end
+
+
+
+
