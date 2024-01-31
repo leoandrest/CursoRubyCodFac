50e6d66fd5e3ec98d25dcf6162c6537ffbc24e69
leo tatico <leoandrest40@gmail.com>
2024-01-31 09:59:20 -0500

    add clase 10

---
+++ clases/operadoresLogicos-clase10.rb
@@ -0,0 +1,54 @@
+#consola IRB
+
+(10 > 5) && (5 < 8)
+=true
+
+(10 > 5) && (5 < 5)
+=> false #si alguna de las dos es falsa dara false.
+
+(10 > 5) || (5 < 5)
+=> true # una de las dos condiciones tiene que dar verdadero para que se cumpla.
+
+(10 > 5) || (ashdhfs)
+=> true
+
+(10 > 5) and (5 < 8)
+=>true
+
+(10 > 5) or (ashdhfs)
+=> true
+
+!true
+=>false 
+
+!false
+=>true
+
+not true
+=>false 
+not false
+=> true
+
+#operadores con mas precedencia(importancia)
+
+1 ** exponente
+2 ! negacion
+3 * / %
+4 + - 
+5 <> <= >= comparacion
+6 == <=> !=
+7 &
+8 ||
+9 asignacion
+10 not
+11 or and 
+
+ej:
+
+10 > 5 %% 5 < 8
+=> true  (signo de precedencia de comparacion)
+
+!true %% true 
+=> false (signo de precedencia de negacion)
+
+
